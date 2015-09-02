07f53d2395c2f70449bd27e142334ab5e354c0b1
Philippe Vinchon <p.vinchon@gmail.com>
2015-09-02 14:58:17 +0100

    Add + method to grid

--- model/src/main/scala/SudokuGrid.scala
+++ model/src/main/scala/SudokuGrid.scala
@@ -1,7 +1,18 @@
 package com.github.plippe.sudokusolver.model
 
 case class SudokuGrid(
-  cells: Seq[SudokuCell])
+    cells: Seq[SudokuCell]) {
+
+  def +(cell: SudokuCell): SudokuGrid = this + SudokuGrid(Seq(cell))
+  def +(cells: SudokuCell*): SudokuGrid = this + SudokuGrid(cells)
+  def +(that: SudokuGrid): SudokuGrid = {
+    val cells: Seq[SudokuCell] = this.cells ++ that.cells
+    val cellsByRowColumn: Iterable[Seq[SudokuCell]] = cells.groupBy { c => s"${c.row}:${c.column}" }.values
+    val cellsSummed: Iterable[SudokuCell] = cellsByRowColumn.map(_.reduce(_ + _))
+
+    new SudokuGrid(cellsSummed.toSeq)
+  }
+}
 
 object SudokuGrid {
   def empty(): SudokuGrid = {
--- model/src/test/scala/TestSudokuGrid.scala
+++ model/src/test/scala/TestSudokuGrid.scala
@@ -20,5 +20,24 @@ class TestSudokuGrid extends FunSpec {
         }
       }
     }
+
+    describe("sum") {
+      val empty = SudokuGrid.empty()
+
+      it("should sum cells to return at most one element per cell") {
+        val grid = empty +
+          SudokuCellUnknown(0, 0, Set(1, 2, 3)) +
+          SudokuCellUnknown(0, 0, Set(2, 3, 4)) +
+          SudokuCellKnown(1, 1, 1)
+
+        val elementsAtR0C0 = grid.cells.filter { cell => cell.row == 0 && cell.column == 0 }
+        assert(elementsAtR0C0.length === 1)
+        assert(elementsAtR0C0.head.asInstanceOf[SudokuCellUnknown].values === Set(2, 3))
+
+        val elementsAtR1C1 = grid.cells.filter { cell => cell.row == 1 && cell.column == 1 }
+        assert(elementsAtR1C1.length === 1)
+        assert(elementsAtR1C1.head.asInstanceOf[SudokuCellKnown].value === 1)
+      }
+    }
   }
 }
