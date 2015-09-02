fcd69647f168eb6a3b6319c247de0172dcafb5b9
Philippe Vinchon <p.vinchon@gmail.com>
2015-09-02 18:06:03 +0100

    Add recursive solve to resolve sudoku grid

--- model/src/main/scala/SudokuGrid.scala
+++ model/src/main/scala/SudokuGrid.scala
@@ -1,5 +1,7 @@
 package com.github.plippe.sudokusolver.model
 
+import scala.annotation.tailrec
+
 case class SudokuGrid(
     cells: Seq[SudokuCell]) {
 
@@ -11,6 +13,51 @@ case class SudokuGrid(
     val cellsSummed: Iterable[SudokuCell] = cellsByRowColumn.map(_.reduce(_ + _))
 
     new SudokuGrid(cellsSummed.toSeq)
+  }
+
+  def solved(): SudokuGrid = {
+    def removeKnowValue(cells: Seq[SudokuCell], original: SudokuCellKnown): SudokuGrid = {
+      def removeValue(cell: SudokuCellUnknown) =
+        SudokuCell(cell.row, cell.column, cell.values - original.value)
+
+      val unknownCells: Seq[SudokuCellUnknown] = cells
+        .filter(_.isInstanceOf[SudokuCellUnknown])
+        .map(_.asInstanceOf[SudokuCellUnknown])
+
+      SudokuGrid(unknownCells.filter(_.row == original.row).map(removeValue)) +
+        SudokuGrid(unknownCells.filter(_.column == original.column).map(removeValue)) +
+        SudokuGrid(unknownCells.filter(_.square == original.square).map(removeValue))
+    }
+
+    def couldValueBeElsewhere(cells: Seq[SudokuCell], value: Int, original: SudokuCellUnknown): Boolean = {
+      val unknownCells: Seq[SudokuCellUnknown] = cells
+        .filter(_.isInstanceOf[SudokuCellUnknown])
+        .map(_.asInstanceOf[SudokuCellUnknown])
+        .filterNot(_ == original)
+
+      unknownCells.exists { cell => !cell.values.contains(value) && cell.row == original.row } ||
+        unknownCells.exists { cell => !cell.values.contains(value) && cell.column == original.column } ||
+        unknownCells.exists { cell => !cell.values.contains(value) && cell.square == original.square }
+    }
+
+    @tailrec def recursive(grid: SudokuGrid): SudokuGrid = {
+      val grids: Seq[SudokuGrid] = grid.cells.map {
+        case cell: SudokuCellKnown => removeKnowValue(grid.cells, cell)
+        case cell: SudokuCellUnknown =>
+          val optValue: Option[Int] = cell.values.find(couldValueBeElsewhere(grid.cells, _, cell))
+          val newCell: SudokuCell = optValue.fold[SudokuCell](cell) { value => SudokuCellKnown(cell.row, cell.column, value) }
+          SudokuGrid(Seq(newCell))
+      }
+
+      grid + grids.reduce(_ + _) match {
+        case newGrid if !newGrid.cells.exists(_.isInstanceOf[SudokuCellUnknown]) => newGrid
+        case newGrid if newGrid == grid => newGrid
+        case newGrid => recursive(newGrid)
+      }
+    }
+
+    val gridWithAllCells = SudokuGrid.empty + this
+    recursive(gridWithAllCells)
   }
 }
 
--- model/src/test/scala/TestSudokuGrid.scala
+++ model/src/test/scala/TestSudokuGrid.scala
@@ -39,5 +39,94 @@ class TestSudokuGrid extends FunSpec {
         assert(elementsAtR1C1.head.asInstanceOf[SudokuCellKnown].value === 1)
       }
     }
+
+    describe("solved") {
+      it("should fill an empty grid") {
+        val grid = SudokuGrid(Seq()).solved
+        assert(grid.cells.length === 81)
+      }
+
+      it("should remove a value from all cells in a row") {
+        val cells = Seq(SudokuCellKnown(0, 0, 1))
+        val grid = SudokuGrid(cells).solved
+
+        for {
+          r <- 0.to(8)
+          genericCell <- grid.cells.find { cell => cell.row == r && cell.column == 0 }
+          if genericCell.isInstanceOf[SudokuCellUnknown]
+          cell = genericCell.asInstanceOf[SudokuCellUnknown]
+        } {
+          assert(!cell.values.contains(1))
+        }
+      }
+
+      it("should remove a value from all cells in a column") {
+        val cells = Seq(SudokuCellKnown(0, 0, 1))
+        val grid = SudokuGrid(cells).solved
+
+        for {
+          c <- 0.to(8)
+          genericCell <- grid.cells.find { cell => cell.row == 0 && cell.column == c }
+          if genericCell.isInstanceOf[SudokuCellUnknown]
+          cell = genericCell.asInstanceOf[SudokuCellUnknown]
+        } {
+          assert(!cell.values.contains(1))
+        }
+      }
+
+      it("should remove a value from all cells in a square") {
+        val cells = Seq(SudokuCellKnown(0, 0, 1))
+        val grid = SudokuGrid(cells).solved
+
+        for {
+          r <- 0.to(2)
+          c <- 0.to(2)
+          genericCell <- grid.cells.find { cell => cell.row == r && cell.column == c }
+          if genericCell.isInstanceOf[SudokuCellUnknown]
+          cell = genericCell.asInstanceOf[SudokuCellUnknown]
+        } {
+          assert(!cell.values.contains(1), cell)
+        }
+      }
+
+      it("should set a value if no cell can in a row") {
+        val values = Set(1, 2, 3, 4, 5, 6, 7, 8)
+        val base = for { r <- 0.to(8) } yield { SudokuCellUnknown(r, 0, values) }
+        val cells = base.filterNot { c => c.row == 0 && c.column == 0 } :+ SudokuCellUnknown(0, 0)
+
+        val grid = SudokuGrid(cells).solved
+        val cell = grid.cells.find { cell => cell.row == 0 && cell.column == 0 }.get
+
+        assert(grid.cells.length === 81)
+        assert(cell.isInstanceOf[SudokuCellKnown])
+        assert(cell.asInstanceOf[SudokuCellKnown].value === 9)
+      }
+
+      it("should set a value if no cell can in a column") {
+        val values = Set(1, 2, 3, 4, 5, 6, 7, 8)
+        val base = for { c <- 0.to(8) } yield { SudokuCellUnknown(0, c, values) }
+        val cells = base.filterNot { c => c.row == 0 && c.column == 0 } :+ SudokuCellUnknown(0, 0)
+
+        val grid = SudokuGrid(cells).solved
+        val cell = grid.cells.find { cell => cell.row == 0 && cell.column == 0 }.get
+
+        assert(grid.cells.length === 81)
+        assert(cell.isInstanceOf[SudokuCellKnown])
+        assert(cell.asInstanceOf[SudokuCellKnown].value === 9)
+      }
+
+      it("should set a value if no cell can in a square") {
+        val values = Set(1, 2, 3, 4, 5, 6, 7, 8)
+        val base = for { r <- 0.to(2); c <- 0.to(2) } yield { SudokuCellUnknown(r, c, values) }
+        val cells = base.filterNot { c => c.row == 0 && c.column == 0 } :+ SudokuCellUnknown(0, 0)
+
+        val grid = SudokuGrid(cells).solved
+        val cell = grid.cells.find { cell => cell.row == 0 && cell.column == 0 }.get
+
+        assert(grid.cells.length === 81)
+        assert(cell.isInstanceOf[SudokuCellKnown])
+        assert(cell.asInstanceOf[SudokuCellKnown].value === 9)
+      }
+    }
   }
 }
